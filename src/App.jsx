d6d932b59ae67797af8f06ec08d260187e9cc436
danishkodemonkey <humberjini@hotmail.com>
2024-04-08 14:14:45 +0200

    BREAKING: add localstorage persistence

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
 import Experience from './components/Experience.jsx';
 import PersonalInformation from './components/PersonalInfo.jsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import './styles/App.css';
 function App() {
-    /* function to handle saving, placeholder to console for now */
     const [personData, setPersonData] = useState([]);
     const [workExperienceData, setWorkExperienceData] = useState([]);
     const [EducExperienceData, setEducExperienceData] = useState([]);
+
+    useEffect(() => {
+        // check if there is saved data in local storage
+        const savedData = localStorage.getItem('Curriculum-Vitae');
+        if (savedData) {
+            console.log('Saved data found, parsing...');
+            const parsedData = JSON.parse(savedData);
+            setPersonData(parsedData['Personal information'] || []);
+            setWorkExperienceData(parsedData['Work experience'] || []);
+            setEducExperienceData(parsedData['Educational experience'] || []);
+        }
+        /* Run once, thanks internet */
+    }, []);
     const handleSave = () => {
         const jsonData = JSON.stringify({
             'Personal information': personData,
             'Work experience': workExperienceData,
             'Educational experience': EducExperienceData,
         });
-        console.log('Data saved!');
-        console.log('================================');
-        console.log(jsonData);
+        console.log('Data saved to local storage!');
+        localStorage.setItem('Curriculum-Vitae', jsonData);
     };
+
     return (
         <form
             action=''
             id='CV'
         >
             <h1>Curriculum Vitae</h1>
+            {/* save button */}
+            <button
+                type='button'
+                className='save-button'
+                onClick={handleSave}
+            >
+                Save
+            </button>
             <section>
                 <PersonalInformation
                     personData={personData}
@@ -42,13 +63,6 @@ function App() {
                     setExperienceData={setEducExperienceData}
                 />
             </section>
-            {/* save button */}
-            <button
-                type='button'
-                onClick={handleSave}
-            >
-                Save
-            </button>
         </form>
     );
 }
